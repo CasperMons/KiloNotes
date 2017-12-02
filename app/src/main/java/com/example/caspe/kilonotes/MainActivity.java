477f1b7b4ccec432d13c6f7c25b88e6be97b8f4a
Casper Mons PC <casper.mons@hva.nl>
2017-12-02 16:16:38 +0100

    Updated layout of mainActivity. New feature: auto calculate distance when typing start and end

--- app/src/main/java/com/example/caspe/kilonotes/MainActivity.java
+++ app/src/main/java/com/example/caspe/kilonotes/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.caspe.kilonotes;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -12,7 +14,7 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView drivenKm;
+    TextView drivenDistance;
     EditText startDistance;
     EditText endDistance;
 
@@ -24,26 +26,74 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        declareLayoutElements();
 
+        startDistance.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                drivenDistance.setText("0 KM");
+            }
 
+            @Override
+            public void onTextChanged(CharSequence s, int i, int i1, int i2) {
+                if(!s.equals(""))
+                {
+                    setDrivenKm();
+                }
+            }
 
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        endDistance.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                drivenDistance.setText("0 KM");
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int i, int i1, int i2) {
+                if(!s.equals(""))
+                {
+                    setDrivenKm();
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
 
+
+
     public void declareLayoutElements(){
-        drivenKm = (TextView)findViewById(R.id.driven_km_txt);
-        startDistance = (EditText)findViewById(R.id.start_text);
+        drivenDistance = (TextView)findViewById(R.id.driven_km_txt);
+        startDistance = (EditText)findViewById(R.id.edit_start_dist);
         endDistance = (EditText)findViewById(R.id.edit_end_dist);
     }
 
-    public void setDrivenKm(int startDistance, int endDistance){
-        int drivenDistance = endDistance - startDistance;
-        drivenKm.setText(Integer.toString(drivenDistance));
-        // Todo: set drivenKm when editText's change
+    public void setDrivenKm(){
+        if(!(endDistance.getText().toString().matches(""))&& !(startDistance.getText().toString().matches(""))) {
+            int startKm = Integer.valueOf(startDistance.getText().toString());
+            int endKm = Integer.valueOf(endDistance.getText().toString());
+
+            int drivenKm = endKm - startKm;
+            if(drivenKm > 0){
+                drivenDistance.setText(Integer.toString(drivenKm) + " KM");
+            }
+        }
     }
 
     public void saveAction(View view){
 
     }
+
 
     public void autoFillStartDistance(View view){
 
