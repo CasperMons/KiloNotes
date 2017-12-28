51b017015eae921bc7a1ac0e3f489250e0598e79
Casper <casper.mons@hva.nl>
2017-12-28 13:25:14 +0100

    Included FirebaseAuthentication package, re-styled LoginScreen and added a registerUser button

--- app/src/main/java/com/example/caspe/kilonotes/activities/LoginActivity.java
+++ app/src/main/java/com/example/caspe/kilonotes/activities/LoginActivity.java
@@ -10,15 +10,27 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.caspe.kilonotes.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+
     }
 
     public void loginAction(View view){
